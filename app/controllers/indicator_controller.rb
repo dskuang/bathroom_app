f0da095029ef3b53c6f394c1c4b27745ecb13cb1
Dominic Kuang <dskuang@gmail.com>
2017-09-24 15:48:04 -0700

    add authentication for controllers

--- app/controllers/indicator_controller.rb
+++ app/controllers/indicator_controller.rb
@@ -1,4 +1,6 @@
 class IndicatorController < ApplicationController
+  before_action :authenticate_request, only: [:occupied, :vacant]
+
   def index
     @indicator = indicator
     @status = indicator.status
@@ -29,5 +31,17 @@ class IndicatorController < ApplicationController
 
   def broadcast_status
     ActionCable.server.broadcast('updates', status: @status)
+  end
+
+  def authenticate_request
+    indicator_options[:key] == auth_token
+  end
+
+  def auth_token
+    RequestToken.last.authentication
+  end
+
+  def indicator_options
+    params.require(:options).permit(:key)
   end
 end
